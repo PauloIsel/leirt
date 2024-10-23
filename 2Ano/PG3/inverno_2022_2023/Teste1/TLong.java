a07615e07204b9f9c8ed001776f4a74cd57ff88c
A51702 <A51702@alunos.isel.pt>
2024-10-23 19:30:15 +0100

    teste 2223

--- 2Ano/PG3/inverno_2022_2023/Teste1/TLong.java
+++ 2Ano/PG3/inverno_2022_2023/Teste1/TLong.java
@@ -3,7 +3,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.function.Predicate;
 
-public abstract class TLong extends Transaction implements Composition{
+public class TLong extends Transaction implements Composition{
     private final List<Transaction> Transactions= new ArrayList<>();
 
     public TLong(String name){
@@ -30,5 +30,48 @@ public abstract class TLong extends Transaction implements Composition{
             if(t.find(pred)) return true;
         }
         return false;
+    }
+
+    public String getDescription(String prefix){
+        StringBuilder s = new StringBuilder(super.getDescription(prefix));
+        for (Transaction t : Transactions) {
+            s.append("\n").append(t.getDescription("  " + prefix));
+//            s.append("\n").append("aaa");
+        }
+        return s.toString();
+    }
+
+    public String compensationToString() {
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+        TLong travel = new TLong("Viagem");
+        try {
+            TLong flight= new TLong("Marcar voo");
+            TShort book = new TShort("Reservar voo", "Cancelar voo"),
+                    pay = new TShort("Pagamento voo", "Devolver pagamento voo");
+            flight.append(book).append( pay );
+            TLong hotel = new TLong("Marcar hotel");
+            hotel.append( new TShort("Reservar hotel", "Cancelar hotel") )
+                    .append( new TShort("Pagamento estadia","Devolver pagamento estadia"));
+
+            travel.append( flight ).append( hotel );
+            travel.append( book);
+        }
+        catch ( TransactionException e ) {
+            System.out.println( e.getMessage() );
+            System.out.println( e.getTransaction() );
+        }
+        if( ! travel.find(s-> s.equals("Viagem")) &&
+                travel.find(s-> s.equals("Reservar hotel")) &&
+                travel.find(s-> s.equals("Cancelar voo")) )
+            System.out.println( "Find is CORRECT" );
+
+        System.out.println( travel.getDescription("-> ") );
+
+        System.out.println( travel.compensationToString() );
+
     }
 }
